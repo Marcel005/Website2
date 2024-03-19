9def253da99bdb0976338547d17d384efa6ec831
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-03-19 17:17:20 +0000

    Tue, Mar 19, 2024, 10:17 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,7 +4,16 @@ close = document.getElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
-
+// create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+    
+}
 
 
 
