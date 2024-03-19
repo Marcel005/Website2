40f380b53a518279bedc5b433c57a0e3251367e0
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-03-19 17:07:47 +0000

    Tue, Mar 19, 2024, 10:07 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,10 +1,12 @@
 show = document.getElementById('rules-btn')
 close = document.getElementById('close-btn')
 
+//open rules
 show.addEventListener('click', () => {
     rules.classList.add('show')
 })
 
+//close rules
 close.addEventListener('click', () => {
     rules.classList.remove('show')
 })
