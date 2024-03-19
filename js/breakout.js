6be568a0e02a8b6bce575dd278ae75ae6edfb249
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-03-19 17:31:09 +0000

    Tue, Mar 19, 2024, 10:31 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -14,11 +14,23 @@ ball = {
     dy: -4,
 }
 
+//create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
 //draw a ball on canvas
 function drawBall() {
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
-    ctx.stroke()
+    ctx.fillStyle = 'green'
+    ctx.fill()
+    ctx.closePath()
 }
 
 
