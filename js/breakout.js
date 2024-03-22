13e02028167737000a69408627d9804f636fb955
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-03-22 16:36:46 +0000

    Fri, Mar 22, 2024, 9:36 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,11 @@ close = document.getElementById('close-btn')
 canvas = document.getElementById('canvas')
 ctx = canvas.getContext('2d')
 
+score = 0
+
+brickRowCount = 9
+brickColumnCount = 5
+
 // create ball properties
 ball = {
     x: canvas.width / 2,
@@ -24,6 +29,27 @@ paddle = {
     dx: 0,
 }
 
+//create brick properties
+brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true,
+}
+
+// create bricks
+bricks = []
+for (let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for (let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
+        bricks[i][j] = {x, y, ...brickInfo}
+    }
+}
+
 //draw a ball on canvas
 function drawBall() {
     ctx.beginPath();
@@ -33,8 +59,43 @@ function drawBall() {
     ctx.closePath()
 }
 
+//draw paddle on canvas
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = 'brown'
+    ctx.fill()
+    ctx.closePath()
+}
 
-drawBall()
+//draw score on canvas
+function drawScore() {
+    ctx.font ='20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
+}
+
+// draw bricks on canvas
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? 'orange' : 'transparent';
+            ctx.fill()
+            ctx.closePath()
+        })
+    })
+}
+
+//draw everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+    drawBricks()
+}
+
+draw()
 
 
 
