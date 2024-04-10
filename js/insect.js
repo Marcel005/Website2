50b0c2a128191b799ce927c80189f6b2aeb47f7e
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-04-10 16:40:06 +0000

    Wed, Apr 10, 2024, 9:40 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -52,6 +52,9 @@ function startGame() {
 function increaseTime() {
     let m = Math.floor(seconds / 60)
     let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
+    }
 }
 
 function addInsects() {
