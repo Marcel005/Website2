04f2834c38fdbcf53c1a4a558e9b667eddecab2e
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-04-10 16:42:49 +0000

    Wed, Apr 10, 2024, 9:42 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -55,6 +55,11 @@ function increaseTime() {
     if (m < 10) {
         m = `0${m}`
     }
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds++
 }
 
 function addInsects() {
