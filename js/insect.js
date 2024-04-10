556af2bc3bc42f331c62326552224d359f2cafb3
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-04-10 15:53:32 +0000

    Wed, Apr 10, 2024, 8:53 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,4 +1,10 @@
 const screens = document.querySelectorAll('.screen')
-const choose_insect_btns = querySelector
-
-screens[0].classList.add('up')
+const choose_insect_btns = querySelectorAll('.chooose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+let seconds = 0
+let score = 0
+let selected_insect = {}
