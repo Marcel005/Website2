cb9d0b7ccffc5f72733d1adb538782c587455800
Marcel Leal <142708094+Marcel005@users.noreply.github.com>
2024-04-22 16:56:42 +0000

    Mon, Apr 22, 2024, 9:56 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -6,9 +6,28 @@ const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
-const word = ['application', 'programming', 'interface', 'Lebron']
+const word = ['HogRider', 'Larry', 'MegaKnight', 'Lebron']
 
 let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+//show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+        ${selectedWord
+            .split('')
+            .map(letter => `
+                <span class="letter">
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>
+            `)}
+
+    `
+}
+
+displayWord()
 
 
